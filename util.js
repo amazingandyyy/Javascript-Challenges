138cdb3d37b49176f52201b5bc565162fe8b670a
march-dave <ysangy@gmail.com>
2016-01-16 08:48:44 -0500

    Util Js

---
+++ util.js
@@ -0,0 +1,13 @@
+
+//superCounter("Count me in");
+//{"words": 3, "chars": 11, "avgLength": 3, "spaces": 2}
+var paramText = "Count me in";
+var superCounter = function(paramText) {
+
+ //var numStr = paramText.toString();
+ var numStr = paramText.toString().length; // chars
+
+return  numStr;
+}
+
+console.log(superCounter(paramText));
