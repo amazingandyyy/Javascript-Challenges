4d7a7a8181755a32d934a8519aec89e5e6e32bbd
march-dave <ysangy@gmail.com>
2016-01-19 07:55:59 -0500

    replace var to any sentence

--- Challenge4.js
+++ Challenge4.js
@@ -7,3 +7,33 @@
 // Would return:
 //
 // "lIFE IS 10% WHAT HAPPENS TO YOU, AND 90% OF HOW YOU react TO IT"
+
+
+
+//var sentence = "Life is 10% what happens to you, and 90% of how you REACT to it";
+function swapCase(sentence) {
+
+var i = 0;
+var character = '';
+var output = '';
+
+while(i<= sentence.length) {
+
+character = sentence.charAt(i);
+
+if (character === character.toUpperCase()) {
+  character = character.toLowerCase();
+}
+else {
+ character = character.toUpperCase();
+}
+
+output = output.concat(character);
+i++;
+}
+
+  return output;
+}
+
+// console.log(swapCase(sentence));
+console.log(swapCase("Life is 10% what happens to you, and 90% of how you REACT to it"));
