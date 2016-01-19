2e74bdfb2c9bac6bf3f0d39c69674515bc5c93c3
march-dave <ysangy@gmail.com>
2016-01-18 20:16:31 -0500

    Challenge 3

---
+++ Challenge4.js
@@ -0,0 +1,9 @@
+// Challenge 4
+// Write a JavaScript function that would swap the case of each character in the string you pass to it as an argument.
+//
+// Test Case
+//
+// swapCase("Life is 10% what happens to you, and 90% of how you REACT to it");
+// Would return:
+//
+// "lIFE IS 10% WHAT HAPPENS TO YOU, AND 90% OF HOW YOU react TO IT"
