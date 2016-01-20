a100689a972203e80d35b4ba4a7cdbddb8053304
march-dave <ysangy@gmail.com>
2016-01-19 21:54:09 -0500

    function to object switch

--- Challenge3.js
+++ Challenge3.js
@@ -1,16 +1,34 @@
 //superCounter("Count me in");
 //{"words": 3, "chars": 11, "avgLength": 3, "spaces": 2}
-var paramText = "Count me in";
-var superCounter = function(paramText) {
+// var paramText = "Count me in";
+// var superCounter = function(paramText) {
+//
+// var names = paramText.split(" ");
+//
+// var words = names.length;
+// var chars = paramText.toString().length; // chars
+// var spaces = names.length - 1;
+// var avgLength = (chars - spaces) / words;
+//
+// return "words : " + words +  ", chars : " + chars + ", avgLength : " + avgLength + ", spaces: " + spaces  ;
+// }
+//
+// console.log(superCounter(paramText));
 
-var names = paramText.split(" ");
+var Util = {
 
-var words = names.length;
-var chars = paramText.toString().length; // chars
-var spaces = names.length - 1;
-var avgLength = (chars - spaces) / words;
+// Util.paramText = "Count me in";
+superCounter : function(paramText) {
+  var names = paramText.split(" ");
 
-return "words : " + words +  ", chars : " + chars + ", avgLength : " + avgLength + ", spaces: " + spaces  ;
+  var words = names.length;
+  var chars = paramText.toString().length; // chars
+  var spaces = names.length - 1;
+  var avgLength = (chars - spaces) / words;
+
+  return "words : " + words +  ", chars : " + chars + ", avgLength : " + avgLength + ", spaces: " + spaces  ;
 }
 
-console.log(superCounter(paramText));
+}
+
+console.log(Util.superCounter("Count me in"));
