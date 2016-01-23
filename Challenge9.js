cedb58818d0925e9ebd360180e1603c2c844a6d6
march-dave <ysangy@gmail.com>
2016-01-23 17:06:32 -0500

    Start Challenge 9 Word Reverser

---
+++ Challenge9.js
@@ -0,0 +1,15 @@
+// Write a JavaScript function that takes one argument that is a sentence (multiple words) and returns a new sentence where all words are reversed but kept in the same order as the original sentence.
+//Test Case
+// wordsReverser("This is fun, hopefully.");
+//Would return:
+//"sihT si nuf, yllufepoh."
+
+function wordsReverser(param) {
+
+  var array = param.split(" ");
+
+
+
+
+
+}
