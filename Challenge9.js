1053e47b0f59194e06d46f114911b9ad5a518c53
march-dave <ysangy@gmail.com>
2016-01-25 07:11:10 -0500

    wordsReverser update

--- Challenge9.js
+++ Challenge9.js
@@ -4,25 +4,65 @@
 //Would return:
 //"sihT si nuf, yllufepoh."
 
-function wordsReverser(param) {
+// wordsReverser
 
-  var array = param.split(" ");
+function wordsReverser(str) {
 
-  var reverseArray = wordsArray.map(function(string) {
+// return error if str is not a string
 
-  var word = string.split("");
+if (typeof str !== "string") {
 
-  var punctuation = word.pop(); //
+return "wordsReverser(str) requires a string input";
 
-  var reversedWord = word.reverse();
+}
 
-  reversedWord.push(punctuation);
+// split words (punctuation inclusive) and numbers while filtering out extra spaces
 
-  // return joined word
+var wordsArray = str.match(/[a-z.,-\/#!$%\^&\*;:{}=\_`~()1-90]+/gi);
 
-  return reversedWord.join("");
+// create a mapped array of reversed words
 
-  }
+var reverseArray = wordsArray.map(function(string) {
+
+// split each word array into individual character arrays that include punctuation.
+
+var word = string.split("");
+
+// if character array includes punctuation characters at last index
+
+if (/[.,?!]+/g.test(word.slice(string.length -1))) { // <<<------- punctuation marks in this regex will maintain their original index in the return string
+
+// pop off punctuation mark and store it
+
+var punctuation = word.pop(); //
+
+// reverse remaining characters
+
+var reversedWord = word.reverse();
+
+// push punctuation onto the end of reversed characters array
+
+reversedWord.push(punctuation);
+
+// return joined word
+
+return reversedWord.join("");
+
+}
+
+// if there is no punctuation at the end of the word, then simply return the reversed word
+
+else {
+
+return word.reverse().join("");
+
+}
+
+});
+
+// return string of reverseArray joined by spaces
+
+return reverseArray.join(" ");
 
 }
 
