89076d8d8793958673e27b8261882ba1c305c0bb
march-dave <ysangy@gmail.com>
2016-01-27 19:20:55 -0500

    add debug code

--- Challenge9.js
+++ Challenge9.js
@@ -1,20 +1,28 @@
 // Write a JavaScript function that takes one argument that is a sentence (multiple words) and returns a new sentence where all words are reversed but kept in the same order as the original sentence.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
+// ("This is fun, hopefully."); ==> "sihT si nuf, yllufepoh."
+
 function wordsReverser(str) {
 
   var wordsArray = str.match(/[a-z.,-\/#!$%\^&\*;:{}=\_`~()1-90]+/gi);
+  // console.log("wordsArray:" + wordsArray);
+
   var reverseArray = wordsArray.map(function(string) {
 
     var word = string.split("");
+    // console.log("word:" + word);
 
     // if character array includes punctuation characters at last index
     if (/[.,?!]+/g.test(word.slice(string.length -1))) {
       // pop off punctuation mark and store it
       var punctuation = word.pop(); //
+      console.log("punctuation:" + punctuation);
 
       // reverse remaining characters
       var reversedWord = word.reverse();
+      console.log("reversedWord:" + reversedWord);
+
 
       // push punctuation onto the end of reversed characters array
       reversedWord.push(punctuation);
