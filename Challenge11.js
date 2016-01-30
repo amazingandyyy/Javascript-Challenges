870e9fb131e94a5dd09e7df31e20c61bb4be039b
march-dave <ysangy@gmail.com>
2016-01-29 19:40:36 -0500

    Get current data in Javascript

--- Challenge11.js
+++ Challenge11.js
@@ -1,7 +1,7 @@
 function age(year, month, day) {
 
   //var today = new Date(2015, 4, 2);
-  var today =getDate();
+  var today = new Date();
   var targetDate = new Date(year, month, day);
 
   var days = Math.abs(today.getDate() - targetDate.getDate());
