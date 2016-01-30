2e6a6ad89f3b5e284ce48c59e8f82b9e2fb75050
march-dave <ysangy@gmail.com>
2016-01-29 20:53:22 -0500

    To start Challenge 13
    // Write a JavaScript function that takes a sentence as an argument and
    determines which word in that sentence has the greatest number of
    repeated letters (the repeated letters do not have to be consecutive).
    //
    // If the sentence has multiple words with the same max of repeated
    letters, return them all in an Array.

---
+++ Challenge13.js
@@ -0,0 +1,20 @@
+// Write a JavaScript function that takes a sentence as an argument and determines which word in that sentence has the greatest number of repeated letters (the repeated letters do not have to be consecutive).
+//
+// If the sentence has multiple words with the same max of repeated letters, return them all in an Array.
+//
+// Test Case
+//
+// Would select the words:
+// wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale")
+//
+// ["attribute", "success"]
+
+
+
+function wordSelector(sentence) {
+
+  // 1) split sentence to array
+  // 2) to check
+}
+
+console.log(wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale"));
