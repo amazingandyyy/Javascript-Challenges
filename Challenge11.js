2fc88347c98bfbbcb4164c9e2d5cea554d328ce3
march-dave <ysangy@gmail.com>
2016-02-02 07:08:33 -0500

    Date order sequency change
    For Example Date(2016 ,2, 1)  2016 Jan 2nd. It is depend on how to setup
    computer Date type

--- Challenge11.js
+++ Challenge11.js
@@ -1,5 +1,18 @@
 // There is definitely a problem that comes up if you are using absolute values. Test a date that is 364 days from now ("a year from yesterday"). Your function returns that it is one year and one day from now, when in fact it is one day less than a year.
 
+// Write a JavaScript function that takes 3 arguments (year, month, and day) and calculates how many days between that day and today
+//
+// If the days are more than 30, return Y months, Z days
+// If the months are more than 12, return X years, Y months, Z days
+// Test Cases
+//
+// (Assuming today is April 2nd, 2015)
+//
+// age(2017, 11, 17) -> 2 years, 8 months, 15 days
+// age(2015, 3, 6) -> 4 days
+// Note that the month argument starts from 0 (11 is December)
+
+
 function age(year, month, day) {
 
   var today = new Date();
@@ -12,5 +25,6 @@ function age(year, month, day) {
   return years + " years" + " " + months + " months" + " " + days + " days";
 }
 
-console.log(age(2017, 11, 17));
-console.log(age(2015, 3, 6));
+console.log(age(2016, 1, 2));
+// console.log(age(2017, 11, 17));
+// console.log(age(2015, 3, 6));
