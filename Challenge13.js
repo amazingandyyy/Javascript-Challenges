1c03c00afe116b4fd05ca870163d189852387680
march-dave <ysangy@gmail.com>
2016-02-01 21:00:50 -0500

    highist repeat number assign

--- Challenge13.js
+++ Challenge13.js
@@ -1,66 +1,70 @@
 // Write a JavaScript function that takes a sentence as an argument and determines which word in that sentence has the greatest number of repeated letters (the repeated letters do not have to be consecutive).
-//
-// If the sentence has multiple words with the same max of repeated letters, return them all in an Array.
-//
-// Test Case
-//
-// Would select the words:
-// wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale")
-//
-// ["attribute", "success"]
+      //
+      // If the sentence has multiple words with the same max of repeated letters, return them all in an Array.
+      //
+      // Test Case
+      //
+      // Would select the words:
+      // wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale")
+      //
+      // ["attribute", "success"]
 
-function wordSelector(sentence) {
+      function wordSelector(sentence) {
 
-  // 1) split sentence to array
-  // How to: Use split or match Method
-  // match method is more useful one line of code parssing token such as  ".", "–"
-  var array = sentence.match(/[a-z]+/ig);
+          // 1) split sentence to array
+          // How to: Use split or match Method
+          // match method is more useful one line of code parssing token such as  ".", "–"
+          var array = sentence.match(/[a-z]+/ig);
 
-  for(var i=0; i < array.length; i++) {
+          var arrayReturn = [];
+          var highistRepeat = 1;
 
-    //["I","attribute","my","success","to"...]
-    var arrayLetter = array[i];
-    // Repeat letter counter
-    repeatLetterCounter(arrayLetter);
-  }
-  // var mapped = array.map(function(el, i) {
-  //   console.log("mapped: el " + el + "mapped: i " + i);
-  //   // return {
-  //   //   index: i, value: el.toLowerCase()
-  //   // };
-  // })
+          for (var i = 0; i < array.length; i++) {
 
-  // 2) to check
-}
+              //["I","attribute","my","success","to"...]
+              var arrayLetter = array[i];
+              // Repeat letter counter
+              var wordRepeats = repeatLetterCounter(arrayLetter);
 
-function repeatLetterCounter(arrayLetter) {
+              if (wordRepeats > highistRepeat) {
+                  arrayReturn = []; // reset array
+                  arrayReturn.push(arrayLetter);
 
-  var uniqueTokenLetter = "";
-  var repeatLetter = 2;
+                  highistRepeat = wordRepeats;
+              }
+              else if (highistRepeat === wordRepeats) {
+                  arrayReturn.push(arrayLetter);
+              }
+          }
 
-  for(var i=0; i<arrayLetter.length; i++) {
+          return arrayReturn;
+      }
 
-    if (uniqueTokenLetter.indexOf(arrayLetter.charAt(i)) === -1) {
+      function repeatLetterCounter(arrayLetter) {
 
-        // pattern format example: /s/gi
-        var pattern = RegExp(arrayLetter[i], "gi");
+          var uniqueTokenLetter = "";
+          var repeatLetterCount = 2;
 
-        // return matching letter "sucess" ==> ["s", "s", "s"]
-        var arrayMatchingLetter = arrayLetter.match(pattern);
+          for (var i = 0; i < arrayLetter.length; i++) {
 
-        if (arrayMatchingLetter.length > repeatLetter) {
-          repeatLetter = arrayMatchingLetter.length;
-        }
+              if (uniqueTokenLetter.indexOf(arrayLetter.charAt(i)) === -1) {
 
-        uniqueTokenLetter += arrayLetter.charAt(i);
+                  // pattern genarated format example: /s/gi
+                  var pattern = RegExp(arrayLetter[i], "gi");
 
-    }
+                  // return matching letter "sucess" ==> ["s", "s", "s"]
+                  var arrayMatchingLetter = arrayLetter.match(pattern);
 
+                  if (arrayMatchingLetter.length > repeatLetterCount) {
+                      repeatLetterCount = arrayMatchingLetter.length;
+                  }
 
-  }
+                  uniqueTokenLetter += arrayLetter.charAt(i);
+              }
+          }
 
-  return repeatLetter;
+          return repeatLetterCount;
 
-}
+      }
 
-console.log(wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale"));
+       console.log(wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale"));
