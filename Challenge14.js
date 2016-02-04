1fc2bef1d1eb1cfa4e954304258408f218a21bfc
march-dave <ysangy@gmail.com>
2016-02-03 19:51:03 -0500

    Create  function fromCharCode()

--- Challenge14.js
+++ Challenge14.js
@@ -1,15 +1,24 @@
 //Numbers and ASCII
 
 //Base 3, we only can use 0, 1, 2. The first 6 numbers would be:
-0, 1, 2, 10, 11, 12
+// 0, 1, 2, 10, 11, 12
 
+// 20, 21, 22, 30, 31, 32
 //Keep counting, what are the next 6 numbers?
-20, 21, 22, 30, 31, 32
 
 
-// convert it into a sentence.
-var ascii = [73, 32, 115, 112, 101, 97, 107, 32, 105, 110, 32, 110, 117, 109, 98, 101, 114, 115];
 
-ascii.map(function(parameter) {
-	console.log(String.fromCharCode(parameter));
-});
+function fromCharCode() {
+
+  // convert it into a characters.
+  var ascii = [73, 32, 115, 112, 101, 97, 107, 32, 105, 110, 32, 110, 117, 109, 98, 101, 114, 115];
+  var returnCharCode =  ascii.map(function(parameter) {
+    return String.fromCharCode(parameter);
+  });
+
+  return returnCharCode.join("");
+}
+
+var stringCharCode = fromCharCode();
+
+console.log(stringCharCode);
