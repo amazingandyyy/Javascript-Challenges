48284c42bceafb5e32841bf1650df351e2099d90
march-dave <ysangy@gmail.com>
2016-02-05 23:05:03 -0500

    Init
    18. Loops and Iterators

---
+++ Challenge18.js
@@ -0,0 +1,19 @@
+// 18. Loops and Iterators
+// Use a variety of different solutions to find the largest number in an array. Use a while loop, a do..while loop, a for loop, and a forEach loop. Use the following array for each of these loops:
+//
+// [2, 7, 4, 8, 6, 4]
+// Using a for...in loop, console.log all of the keys and values in the following object:
+//
+// { "wheels": 3, "horsepower": 600, "decal": "flames", "awesome": true , "holdsBarred": [] }
+// Using the .map function return an array that is equal to the squares each of the values in the following array:
+//
+// [1, 2, 5, 7, 11]
+// Using the .reduce function find the product of the numbers in the following array:
+//
+// [3, 5, 7, 9, 12]
+// Using the .filter function create a new array of only odd numbers from the following array:
+//
+// [1, 22, 35, 2, 4, 7, 9]
+// Using the .sort function, sort the following array reverse-alphabetically:
+//
+// ['water', 'balloon', 'baseball', 'computer', 'notebook', 'frisbee']
