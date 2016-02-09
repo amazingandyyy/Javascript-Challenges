1a4b8203424d31e250360e263551e90e995966ea
march-dave <ysangy@gmail.com>
2016-02-08 20:13:17 -0500

    Init continue

--- The Game of Hangman/Hangman.js
+++ The Game of Hangman/Hangman.js
@@ -1,10 +1,44 @@
 //Hard code a big array of 100+ source words in your javascript, and pick a word randomly from that.
 
+$(document).ready(function() {
+    $("#btnGuess").click(function() {
+      var guessLetter1 = $("#guessLetter").value;
+      var guessLetter2 = $("#guessLetter").text;
+    }
+
+  );
+
+});
+
 var words = [
-  'X Ray',
-  'MLB'
+  'Olympic',
+  'MLB',
   'FIFA'
 
-
-
 ];
+
+function gameStart(words) {
+  // randomly pick of the words
+  // Random function to use
+  //
+}
+
+function guessWord() {
+
+  // input word
+
+    var letter = $('guessLetter').value;
+
+  // if match word
+
+  // if doesn't match word
+
+  // if match word count reached
+
+  // if doens't match word count
+
+}
+
+// console.log(gameStart(words))
+
+guessWord();
