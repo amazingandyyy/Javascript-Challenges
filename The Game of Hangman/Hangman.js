476fcfa5d838c00f6f746cc67493f05da4ee4185
march-dave <ysangy@gmail.com>
2016-02-12 20:54:42 -0500

    Add gamelist and alphabet list

--- The Game of Hangman/Hangman.js
+++ The Game of Hangman/Hangman.js
@@ -12,22 +12,95 @@ $(document).ready(function() {
 });
 
 var gameList = [
-    { name: 'FIFA', hint: 'Soccer or Football' },
-    { name: 'MLB', hint: 'Baseball' }
+  { name: 'FIFA', hint: 'Soccer or Football' },
+  { name: 'MLB', hint: 'Baseball' },
+  { name: 'Afghanistan', hint: 'Country' },
+  { name: 'Albania', hint: 'Country' },
+  { name: 'Algeria', hint: 'Country' },
+  { name: 'Andorra', hint: 'Country' },
+  { name: 'Angola', hint: 'Country' },
+  { name: 'Argentina', hint: 'Country' },
+  { name: 'Armenia', hint: 'Country' },
+  { name: 'Aruba', hint: 'Country' },
+  { name: 'Australia', hint: 'Country' },
+  { name: 'Austria', hint: 'Country' },
+  { name: 'Azerbaijan', hint: 'Country' },
+
+  { name: 'Bahamas', hint: 'Country' },
+  { name: 'Bahrain', hint: 'Country' },
+  { name: 'Bangladesh', hint: 'Country' },
+  { name: 'Barbados', hint: 'Country' },
+  { name: 'Belarus', hint: 'Country' },
+  { name: 'Belgium', hint: 'Country' },
+  { name: 'Belize', hint: 'Country' },
+  { name: 'Benin', hint: 'Country' },
+  { name: 'Bhutan', hint: 'Country' },
+  { name: 'Bolivia', hint: 'Country' },
+  { name: 'Botswana', hint: 'Country' },
+  { name: 'Brazil', hint: 'Country' },
+  { name: 'Brunei', hint: 'Country' },
+  { name: 'Bulgaria', hint: 'Country' },
+  { name: 'Burma', hint: 'Country' },
+  { name: 'Burundi', hint: 'Country' },
+
+  { name: 'Cambodia', hint: 'Country' },
+  { name: 'Cameroon', hint: 'Country' },
+  { name: 'Canada', hint: 'Country' },
+  { name: 'Cape Verde', hint: 'Country' },
+  { name: 'Chad', hint: 'Country' },
+  { name: 'Chile', hint: 'Country' },
+  { name: 'China', hint: 'Country' },
+  { name: 'Colombia', hint: 'Country' },
+  { name: 'Comoros', hint: 'Country' },
+  { name: 'Congo', hint: 'Country' },
+  { name: 'Costa Rica', hint: 'Country' },
+  { name: 'Croatia', hint: 'Country' },
+  { name: 'Cuba', hint: 'Country' },
+  { name: 'Curacao', hint: 'Country' },
+  { name: 'Cyprus', hint: 'Country' },
+
+  { name: 'Denmark', hint: 'Country' },
+  { name: 'Djibouti', hint: 'Country' },
+  { name: 'Dominica', hint: 'Country' },
+
+  { name: 'Ecuador', hint: 'Country' },
+  { name: 'Egypt', hint: 'Country' },
+  { name: 'El Salvador', hint: 'Country' },
+  { name: 'Equatorial Guinea', hint: 'Country' },
+  { name: 'Eritrea', hint: 'Country' },
+  { name: 'Estonia', hint: 'Country' },
+  { name: 'Ethiopia', hint: 'Country' },
+
+  { name: 'Fiji', hint: 'Country' },
+  { name: 'Finland', hint: 'Country' },
+  { name: 'France', hint: 'Country' },
+
+  { name: 'Gabon', hint: 'Country' },
+  { name: 'Georgia', hint: 'Country' },
+  { name: 'Germany', hint: 'Country' },
+  { name: 'Ghana', hint: 'Country' },
+  { name: 'Greece', hint: 'Country' },
+  { name: 'Grenada', hint: 'Country' },
+  { name: 'Guatemala', hint: 'Country' },
+  { name: 'Guinea', hint: 'Country' },
+  { name: 'Guinea-Bissau', hint: 'Country' },
+  { name: 'Guyana', hint: 'Country' }
+
 ];
 
-// var words = [
-//   'FIFA',
-//   'Olympic',
-//   'MLB'
-// ];
+var alphabetList = {
+  'a', 'b', 'c', 'd', 'e', 'f', 'g',
+  'h', 'i', 'j', 'k', 'l', 'm', 'n',
+  'o', 'p', 'q', 'r', 's', 't', 'u',
+  'v', 'w', 'x', 'y', 'z'
+}
 
 var hangmanObj = "";
 var wordHolder = "";
 function gameStart() {
   // randomly pick of the hangmanObj and init set to "-"
-  // hangmanObj = gameList[Math.floor(Math.random() * gameList.length + 1)];
-  hangmanObj = gameList[0];
+  hangmanObj = gameList[Math.floor(Math.random() * gameList.length)];
+  // hangmanObj = gameList[0];
 
   for (var i=0; i < hangmanObj.name.length; i++) {
     wordHolder+= "-";
