939d9855a99f7277198752d2e20a87d79db89327
march-dave <ysangy@gmail.com>
2016-02-28 15:19:11 -0500

    A checker board start color black

--- Dynamic Element Creation in jQuery/main.js
+++ Dynamic Element Creation in jQuery/main.js
@@ -1,35 +1,9 @@
 $(document).ready(function() {
-  
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-
-
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-  // $(".container #gameBoard").append("<div></div>");
-
 
 	var elements = $();
 	for(x = 0; x < 64; x++) {
-	    elements = elements.add('<div>'+x+'</div>');
-	    // or 
-	    // var element = $('<div>'+x+'</div>');
-	    // elements = elements.add(element);
+	    elements = elements.add('<div></div>');
 	}
 	$('#gameBoard').append(elements);
-
-
-
 
 });
