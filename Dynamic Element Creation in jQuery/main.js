8e1e9242bd6527263a3a042045b1a638048036b9
march-dave <ysangy@gmail.com>
2016-03-01 07:15:49 -0500

    modified:   main.js

--- Dynamic Element Creation in jQuery/main.js
+++ Dynamic Element Creation in jQuery/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-	
 	$('#boardSize').keyup(function() { 
 
 		var size = $('#boardSize').val();
@@ -9,18 +8,9 @@ $(document).ready(function() {
 
 		var elements = $();
 		for(x = 0; x < 64; x++) {
-		    // elements = elements.add('<div style="width:' +  size +'px></div>');
-		      elements = elements.add('<div style=width:' + size + 'px;></div>');
+			elements = elements.add('<div style=width:' + size + 'px;></div>');
 		}
 		$('#gameBoard').append(elements);
 
-		// $("ul.tabbed").css("width", $("ul.tabbed").children("li").size() * 115);
-
 	});
-
 });
-
-
-
-
- 
