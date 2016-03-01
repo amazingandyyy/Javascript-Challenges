b583dfa65be27b65bbbac1ab73c4f0c22eb54ba2
march-dave <ysangy@gmail.com>
2016-02-29 21:13:44 -0500

    css width change dynamically continue

--- Dynamic Element Creation in jQuery/main.js
+++ Dynamic Element Creation in jQuery/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+	
+	$('#boardSize').keyup(function() { 
+
+		var size = $('#boardSize').val();
+
+		console.log('size: ' + size);
+
+	});
+
 	var elements = $();
 	for(x = 0; x < 64; x++) {
 	    elements = elements.add('<div></div>');
