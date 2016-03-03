25c2a7a95b21763b2bd9003c020cb5bfc2d7d5da
march-dave <ysangy@gmail.com>
2016-03-03 07:02:01 -0500

    modified:   main.js 	modified:   index.css

--- Dynamic Element Creation in jQuery/main.js
+++ Dynamic Element Creation in jQuery/main.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
 
 	$('#boardSize').keyup(function() { 
 
-		var size = $('#boardSize').val();
+			var size = $('#boardSize').val();
+			var elements = $();
 
-		var elements = $();
-		for(x = 0; x < 64; x++) {
-			elements = elements.add('<div style=width:' + size + 'px;' +  'height:' + size + 'px;' +'>');
-			elements = elements.add('</div>');
-		}
-		$('#gameBoard').append(elements);
+			// resize css
 
+
+			for(x = 0; x < 64; x++) {
+				elements = elements.add('<div style=width:' + size + 'px;' +  'height:' + size + 'px;' +'>');
+				elements = elements.add('</div>');
+			}
+			$('#gameBoard').append(elements);
+
+		
+		
 	});
 });
