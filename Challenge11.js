af4fd80853f56035ed18cfdf64d8d1b6e7fe9acc
march-dave <ysangy@gmail.com>
2016-03-05 08:28:12 -0500

    feedback update

--- Challenge11.js
+++ Challenge11.js
@@ -13,18 +13,19 @@
 // Note that the month argument starts from 0 (11 is December)
 
 
+// There is definitely a problem that comes up if you are using absolute values. Test a date that is 364 days from now ("a year from yesterday"). Your function returns that it is one year and one day from now, when in fact it is one day less than a year.
+
+
 function age(year, month, day) {
 
   var today = new Date();
   var targetDate = new Date(year, month, day);
 
-  var days = Math.abs(today.getDate() - targetDate.getDate());
-  var months = Math.abs(today.getMonth() - targetDate.getMonth());
-  var years = Math.abs(today.getFullYear() - targetDate.getFullYear());
+  var yearDifference = Math.abs(today.getFullYear() - targetDate.getFullYear());
+  var monthDifference = Math.abs(today.getMonth() - targetDate.getMonth());
+  var dayDifference = Math.abs(today.getDate() - targetDate.getDate());
 
-  return years + " years" + " " + months + " months" + " " + days + " days";
+  return yearDifference + " years" + " " + monthDifference + " months" + " " + dayDifference + " days";
+
+
 }
-
-console.log(age(2016, 1, 2));
-// console.log(age(2017, 11, 17));
-// console.log(age(2015, 3, 6));
