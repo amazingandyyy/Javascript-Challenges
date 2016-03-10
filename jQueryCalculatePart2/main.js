e379174191d7c78e10a48873cfb59acf8bef26fd
march-dave <ysangy@gmail.com>
2016-03-10 06:59:00 -0500

    javascript event add

--- jQueryCalculatePart2/main.js
+++ jQueryCalculatePart2/main.js
@@ -11,14 +11,41 @@ $(document).ready(init);
 function init() {
 	display = $('#display');
 	$('.num').on('click', numClicked);
-	// $('.operator').on('click', operatorClicked);
-	// $('#invert').on('click', invertClicked);
-	// $('#root').on('click', rootClicked);
-	// $('#decimal').on('click', decimalClicked);
-	// $('#equals').on('click', equalsClicked);
-	// $('#clear').on('click', clearClicked);
+	$('.operator').on('click', operatorClicked);
+	$('#invert').on('click', invertClicked);
+	$('#root').on('click', rootClicked);
+	$('#decimal').on('click', decimalClicked);
+	$('#equals').on('click', equalsClicked);
+	$('#clear').on('click', clearClicked);
 }
 
 function numClicked () {
 	alert('Number button clicked!');
 }
+
+function operatorClicked () {
+	alert('Operator button clicked!');
+}
+
+function invertClicked () {
+	alert('Invert button clicked!');
+}
+
+function rootClicked () {
+	alert('Root button clicked!');
+}
+
+function decimalClicked () {
+	alert('Decimal button clicked!');
+}
+
+function equalsClicked () {
+	alert('Equals button clicked!');
+}
+
+function clearClicked () {
+	alert('Clear button clicked!');
+}
+
+
+
