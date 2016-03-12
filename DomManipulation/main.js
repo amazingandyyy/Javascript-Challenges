98fc515b618d3200431a5cd1cb4d050ed6184d63
march-dave <ysangy@gmail.com>
2016-03-12 01:23:10 -0500

    Init

---
+++ DomManipulation/main.js
@@ -0,0 +1,10 @@
+// Use JavaScript to manipulate the DOM
+
+// You may only use Vanilla JavaScript (this means no libraries like jQuery!)
+// Start with the HTML/CSS provided here
+// Don't forget to include Bootstrap!
+// Remove all of the HTML inside of the main container
+// On page load, use JavaScript to create all of these elements. In other words, all of the HTML inside of your container should be added via JavaScript - none of them should exist in your HTML!
+// After the JavaScript executes, the page should look identical to the provided sample HTML
+// On you have created all of the buttons, add click handlers to them. Whenever a button is clicked, replace the text in the <h3> with the contents of the button
+// For example, if the user clicks the "Left" button, the <h3> should then say "Left"
