74e597b91587c07a8417e75d7f8d3ea63964f3da
march-dave <ysangy@gmail.com>
2016-03-14 23:14:50 -0400

    Dynamic loading css

--- DomManipulation/main.js
+++ DomManipulation/main.js
@@ -17,17 +17,56 @@ var buttons = [];
 document.addEventListener('DOMContentLoaded', myFunction);
 
 function myFunction() {
+	var elementDiv = document.createElement('div');
+	elementDiv.className = 'row';
+	document.body.appendChild(elementDiv);	
+
+	// document.body.appendChild(element);	
 	var element = document.createElement('button');
 	element.innerText = 'my Button';
 	element.addEventListener('click',  replaceText);
 
-
-	document.body.appendChild(element);	
+	// document.body.appendChild(element);
+	elementDiv.appendChild(element);
 }
 
 function replaceText() {
 	var elementH3 = document.createElement('h3');
 	elementH3.innerText = "Left"	
 
-	document.body.appendChild(element3);	
+	document.body.appendChild(elementH3);	
 }
+
+// <div class="container">
+//   <div class="row">
+//     <div class="col-md-12">
+//       <h3>Nothing clicked yet!</h3>
+//       <button type="button" class="btn btn-default btn-lg">Star</button>
+//       <hr>
+//       <div>
+//         <div class="btn-group">
+//           <button type="button" class="btn btn-default">1</button>
+//           <button type="button" class="btn btn-default">2</button>
+//           <button type="button" class="btn btn-default">3</button>
+//           <button type="button" class="btn btn-default">4</button>
+//         </div>
+//         <div class="btn-group">
+//           <button type="button" class="btn btn-default">5</button>
+//           <button type="button" class="btn btn-default">6</button>
+//           <button type="button" class="btn btn-default">7</button>
+//         </div>
+//         <div class="btn-group">
+//           <button type="button" class="btn btn-default">8</button>
+//         </div>
+//       </div>
+//       <hr>
+//       <div>
+//         <div class="btn-group btn-group-lg">
+//           <button type="button" class="btn btn-default">Left</button>
+//           <button type="button" class="btn btn-default">Middle</button>
+//           <button type="button" class="btn btn-default">Right</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
