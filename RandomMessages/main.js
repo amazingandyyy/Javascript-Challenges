e3a801c0c9e120bdbd907fd8a1be229a91d03568
march-dave <ysangy@gmail.com>
2016-03-16 21:51:58 -0400

    Random Message loading

--- RandomMessages/main.js
+++ RandomMessages/main.js
@@ -3,6 +3,7 @@
 // Start by creating an array of 10 random messages (strings)
 // On page load, the page should have the message "Hello!"
 // Beneath that, add a button that says "New Message"
+
 // When the button is clicked, randomly select a new message to display on the screen (this will replace the old message)
 // The button should be able to be clicked as many times as you want - every click will change the message
 
@@ -15,8 +16,15 @@ $(document).ready(init);
 
 function init() {
 	loadMessage();
+	$('#btnNew').on('click',  messageRandomly)
 }
 
 function loadMessage() {
 	$('#loading').text('Hello!');
+}
+
+var randomMessage = '';
+function messageRandomly() {
+	randomMessage = messages[Math.floor(Math.random() * messages.length)];
+	$('#loading').text(randomMessage);
 }
