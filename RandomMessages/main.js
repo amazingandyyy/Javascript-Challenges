4cc87cab7256ddbc70d7e23e6cb9802cb95c1a4f
march-dave <ysangy@gmail.com>
2016-03-16 20:44:48 -0400

    new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ RandomMessages/main.js
@@ -0,0 +1,7 @@
+// Use jQuery to display a random message to the user.
+// * Start by creating an array of 10 random messages (strings)
+// * On page load, the page should have the message "Hello!"
+// * Beneath that, add a button that says "New Message"
+// * When the button is clicked, randomly select a new message to display on the screen (this will replace the old message)
+// * The button should be able to be clicked as many times as you want - every click will change the message
+
