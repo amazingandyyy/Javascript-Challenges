f8c4964707197228bb07e7f04c1f04f8657cbd46
march-dave <ysangy@gmail.com>
2016-03-23 19:53:08 -0400

    Code clean

--- palindromicMap.js
+++ palindromicMap.js
@@ -1,8 +1,6 @@
 function palindromicMap(param) {
 
   // Use the .map function in your solution and don't use any loops.
-  // split and remove punctuation and extra spaces
-  // var array = str.match(/[a-z-]+/ig);
   var array = param.split(" ");
   // console.log("array:" + array);
 
