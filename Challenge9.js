4d6624dc0d12a578526f6073ea9213188e76d103
march-dave <ysangy@gmail.com>
2016-03-23 20:30:02 -0400

    Bug Fix

--- Challenge9.js
+++ Challenge9.js
@@ -1,20 +1,20 @@
 // Write a JavaScript function that takes one argument that is a sentence (multiple words) and returns a new sentence where all words are reversed but kept in the same order as the original sentence.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/str/split
 
 // ("This is fun, hopefully."); ==> "sihT si nuf, yllufepoh."
 
-function wordsReverser(str) {
+function wordsReverser(argument) {
 
-  var wordsArray = str.match(/[a-z.,-\/#!$%\^&\*;:{}=\_`~()1-90]+/gi);
-  // console.log("wordsArray:" + wordsArray);
+  var array = argument.match(/[a-z.]+/gi);
+  // console.log("array:" + array);
 
-  var reverseArray = wordsArray.map(function(string) {
+  var resultArray = array.map(function(str) {
 
-    var word = string.split("");
+    var word = str.split("");
     // console.log("word:" + word);
 
     // if character array includes punctuation characters at last index
-    if (/[.,?!]+/g.test(word.slice(string.length -1))) {
+    if (/[.,?!]+/g.test(word.slice(str.length -1))) {
       // pop off punctuation mark and store it
       var punctuation = word.pop(); //
       console.log("punctuation:" + punctuation);
@@ -33,8 +33,8 @@ function wordsReverser(str) {
 
   });
 
-// return string of reverseArray joined by spaces
-return reverseArray.join(" ");
+// return str of resultArray joined by spaces
+return resultArray.join(" ");
 
 }
 
