38dfb3014df9c91f64c821fe7ca61c504fe57aef
march-dave <ysangy@gmail.com>
2016-03-23 21:38:30 -0400

    modified:   Challenge9.js

--- Challenge9.js
+++ Challenge9.js
@@ -1,40 +1,30 @@
 // Write a JavaScript function that takes one argument that is a sentence (multiple words) and returns a new sentence where all words are reversed but kept in the same order as the original sentence.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/str/split
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 // ("This is fun, hopefully."); ==> "sihT si nuf, yllufepoh."
 
 function wordsReverser(argument) {
 
-  var array = argument.match(/[a-z.]+/gi);
-  // console.log("array:" + array);
+  var argArray = argument.match(/[a-z.,]+/gi);
 
-  var resultArray = array.map(function(str) {
+  var reverseArray = argArray.map(function(str) {
 
-    var word = str.split("");
-    // console.log("word:" + word);
+    var wordArray = str.split("");
 
-    // if character array includes punctuation characters at last index
-    if (/[.,?!]+/g.test(word.slice(str.length -1))) {
-      // pop off punctuation mark and store it
-      var punctuation = word.pop(); //
-      console.log("punctuation:" + punctuation);
+    if (/[.,]+/g.test(wordArray.slice(str.length - 1))) {
+      var dot = wordArray.pop(); //
 
-      // reverse remaining characters
-      var reversedWord = word.reverse();
-      console.log("reversedWord:" + reversedWord);
+      var reversedWord = wordArray.reverse();
 
-      // push punctuation onto the end of reversed characters array
-      reversedWord.push(punctuation);
-      // return joined word
+      reversedWord.push(dot);
       return reversedWord.join("");
     } else {
-      return word.reverse().join("");
+      return wordArray.reverse().join("");
     }
 
   });
 
-// return str of resultArray joined by spaces
-return resultArray.join(" ");
+return reverseArray.join(" ");
 
 }
 
