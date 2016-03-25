ff76c5b93a74d1aecb39e5fa3587535b65f5e5f9
march-dave <ysangy@gmail.com>
2016-03-25 08:03:06 -0400

    init

---
+++ Optional/BinarytoDecimal.js
@@ -0,0 +1,13 @@
+// Create a function that operates on a binary string and converts it to a decimal number
+
+// You can't use JavaScript built-in parsing (parseInt and the like)
+// Make the function available to be called on strings as a method
+// Create a function called "assert_equal" that will determine if the 2 arguments passed to it are equal
+// Use this assert_equal function to test your binaryToDecimal function
+// assert_equal("11001010".binaryToDecimal(), 202);
+// assert_equal("1110".binaryToDecimal(), 14);
+
+function () {
+
+
+}
