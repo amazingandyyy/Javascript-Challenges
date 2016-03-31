e4ee4222a44fb5959841e0242d917d5f5abfbda2
march-dave <ysangy@gmail.com>
2016-03-30 20:03:52 -0400

    binaryToDecimal

--- Optional/main.js
+++ Optional/main.js
@@ -12,7 +12,7 @@ function assert_equal(bin, dec) {
 // binary string to decimal number
 
 	if (bin === dec) {
-		console.log("binaryToDecimal");
+		console.log("binaryToDecimal are equal");
 	}
 }
 
@@ -21,7 +21,40 @@ String.prototype.binaryToDecimal = function() {
 	return this.split('').reverse().reduce(function(x, y, i){
 	      return (y === '1') ? x + Math.pow(2, i) : x;
 	    }, 0);
+
+	// return this;
 }
 
 // console.log(assert_equal("11001010".binaryToDecimal(), 202));
 console.log(assert_equal("1110".binaryToDecimal(), 14));
+
+// function binaryToDecimal( binaryNumber ) {
+        
+//    // decimalNumber represents a summation of digit values.
+//    var decimalNumber = 0;
+
+//    // placeValue represents a power of two.
+//    var placeValue = 1;
+
+//    // Compute each digit value (from right to left).
+//    while ( binaryNumber > 0 ) {
+
+//      // Divide binaryNumber by 10. Store the decimal remainder.
+//      var remainder = binaryNumber % 10;
+
+//      // Determine the remainder digit.
+//      var digitValue = remainder * placeValue;
+
+//      // Add the digitValue to the decimalNumber sum.
+//      decimalNumber = decimalNumber + digitValue;
+
+//      // Increment the placeValue
+//      placeValue *= 2;
+
+//      // Increment to the next digit (left adjacent).
+//      binaryNumber = Math.floor( binaryNumber / 10 );
+//    }
+
+//    // Return the sum of digit values.
+//    return decimalNumber;
+//  }
