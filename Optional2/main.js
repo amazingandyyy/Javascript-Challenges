b5404253a85e0e992a045773d13ee2708579626e
march-dave <ysangy@gmail.com>
2016-03-31 06:53:02 -0400

    Init

---
+++ Optional2/main.js
@@ -0,0 +1,16 @@
+// Create a function decodeStr()
+
+// It will receive a string argument which has 2 parts - a stream of characters and a key - and return the decoded message. Those 2 parts will be separated by the "|" sign in the message.
+
+// Example: "sfGuisOthO12D|2 6 9 12"
+
+// The key is a space-separated list of integers. The decoded message will be the letters positioned on those integers in the original stream, so for "2 6 9 12", we want the letters on positions 2, 6, 9, and 12 (starting the count from 0)
+
+// decodeStr("sfGuisOthO12D|2 6 9 12") // "GOOD";
+// decodeStr("01R34E6D|2 5 7") // "RED";
+
+function decodeStr() {
+
+
+}
+
