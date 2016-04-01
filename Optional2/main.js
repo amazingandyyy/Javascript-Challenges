161e7114c93093a2e7880d97caccf51a3a790293
march-dave <ysangy@gmail.com>
2016-03-31 21:17:04 -0400

    Decoder Str module finish

--- Optional2/main.js
+++ Optional2/main.js
@@ -9,8 +9,20 @@
 // decodeStr("sfGuisOthO12D|2 6 9 12") // "GOOD";
 // decodeStr("01R34E6D|2 5 7") // "RED";
 
-function decodeStr() {
+function decodeStr(str) {
 
+	var array = str.split('|');
+	var value = array[0];
+	var key = array[1];
+	var number = key.split(" ");
 
+	var message = "";
+	for(var i=0; i < number.length; i++) {
+		message += value.charAt(number[i]);
+	}
+
+	return message;
 }
 
+console.log(decodeStr("sfGuisOthO12D|2 6 9 12")); // "GOOD";
+console.log(decodeStr("01R34E6D|2 5 7")); // "RED";
