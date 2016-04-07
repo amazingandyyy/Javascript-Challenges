34eefa0acc7fd231c3848b1c6aa742dbe737a6dc
march-dave <ysangy@gmail.com>
2016-04-06 21:56:56 -0400

    Init

---
+++ Optional4/main.js
@@ -0,0 +1,16 @@
+// 4. Working with Data
+
+// Start from this array of GitHub objects: https://api.github.com/users
+
+// Load it up data in your program using jQuery .ajax (or getJSON)
+
+// Write a program to count the following:
+
+// How many of the listed objects are of type "User" and how many are of type "Organization"
+// How many site admins are there in that list?
+// How many users have a GitHub handle that is 5 characters or shorter?
+
+'use strict';
+
+
+
