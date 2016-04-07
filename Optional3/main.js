2a3b3a65724f9a7f24e24aae50df55e5df4214fc
march-dave <ysangy@gmail.com>
2016-04-06 20:59:44 -0400

    Top use words shown in the table

--- Optional3/main.js
+++ Optional3/main.js
@@ -40,13 +40,6 @@ function getValue() {
 			obj.count = count;	
 			statusArray.push(obj);
 
-			// if (count == 1) {
-			// 	countArray.push(count);
-
-			// } else {
-			// 	countArray.push(count);	
-			// }
-
 			count = 1;
 		}
 
@@ -57,13 +50,18 @@ function getValue() {
 
 function addValue() {
 
-	statusArray.sort(function(a, b) { return a.count - b.count; });
+	statusArray.sort(function(a, b) { return b.count - a.count; });
 
+	var number = 1;
 	$.each(statusArray, function(index, value) {
 
 		$("#left").append("<p>" + value.word + "</p>");
 		$("#right").append("<p>" + value.count + "</p>");
+		number++;
 
+		if (number == 10) {
+			return false;
+		}
 	});
 
 }
