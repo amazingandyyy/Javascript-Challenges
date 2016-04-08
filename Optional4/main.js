cf7092a2293f5421b7aa86aeae9a7048972a4768
march-dave <ysangy@gmail.com>
2016-04-07 21:05:57 -0400

    jQuery ajax add

--- Optional4/main.js
+++ Optional4/main.js
@@ -12,5 +12,39 @@
 
 'use strict';
 
+$(document).ready(init);
+
+function init() {
+	$("#addCard").click(addCardHandler);
+}
+
+function addCardHandler() {
+
+	var $usernameInput = $("#username");
+	var username = $usernameInput.val();
+
+	$.get("http://api.github.com/users/" + username)
+
+	.done(function(data, status) {
+		console.log("data", data);
+		console.log("status", status);
+		var $card = makeCard(data);
+		$("#output").append($card);
+	})
+	.fail(function(data, status) {
+		console.log("data", data);
+		console.log("status", status);
+	})
+}
+
+
+function makeCard(data) {
+
+	var $card = $('<div>').addClass('card').css('width', '150px');
+	var $avatar = $('<img>').attr('src', data.avatar_url);
+	var $name = $('<div>').text(data.name);
+	$card.append($avatar).append($name);
+	return $card;
+}
 
 
